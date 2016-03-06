682b59fbcf916646682d852aac043a977e448dae
Casey Rieder <casey.rieder@gmail.com>
2016-03-06 14:36:55 -0500

    Added material-ui & started designing appbar

--- client/modules/core/components/MainLayout.jsx
+++ client/modules/core/components/MainLayout.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Grid, Row} from 'react-bootstrap';
-import Navigation from './Navigation.jsx';
+import AppMenuBar from './AppMenuBar.jsx';
 // Render main layout component w/react-bootstrap styles
 const Layout = ({content = () => null }) => (
   <div>
-    <Navigation />
+    <AppMenuBar />
     <Grid>
       <Row>
       	<h1>My Inspections(for now)</h1>
